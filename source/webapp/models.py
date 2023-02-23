830d310b9e7739f3175031f2f8dad3e2d0a3705a
Nurmuhamed Bekinulu <nurmuhamedbekinulu@gmail.com>
2023-02-23 22:00:11 +0600

    создания модели для задачи

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,35 +2,12 @@ from django.db import models
 
 
 # Create your models here.
-class Article(models.Model):
+class Task(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False, verbose_name="Заголовок")
-    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Текст")
-    author = models.CharField(max_length=200, null=False, blank=False, verbose_name="Автор")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время и дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время и дата обновления")
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Описание")
+    status = models.CharField(max_length=200, null=False, blank=False, verbose_name="Статус")
+    completion_date = models.DateField(auto_now=True, verbose_name="Выполнить до")
 
 
     def __str__(self):
-        return f"{self.author} - {self.title}"
-
-
-class Meta:
-    verbose_name = 'Статья'
-    verbose_name_plural = 'Статьи'
-
-class Comment(models.Model):
-    article = models.ForeignKey(
-        to='webapp.Article',
-        verbose_name='Статья',
-        null=False,
-        blank=False,
-        related_name='comments',
-        on_delete=models.RESTRICT
-        )
-    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Текст")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время и дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время и дата обновления")
-
-
-    def __str__(self):
-        return f"{self.text} - {self.created_at}"
+        return f"{self.title} - {self.description} - {self.completion_date}"
